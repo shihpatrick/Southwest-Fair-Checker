a140f853eff2490b8289e8da9ed8b749b81c4305
shihpatrick <patrickkshih@gmail.com>
2017-09-20 17:58:12 -0700

    develop flask framework

--- check_in.py
+++ check_in.py
@@ -4,27 +4,36 @@ from selenium.webdriver.common.by import By
 
 import sys
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
 def main(argv):
-	number = argv[0]
-	first_name = argv[1]
-	last_name = argv[2]
+  return
+  # number = argv[0]
+  # first_name = argv[1]
+  # last_name = argv[2]
 
-	driver = webdriver.Chrome('/Users/patrickkshih/Desktop/projects/automateproj/chromedriver')
-	driver.get("https://www.southwest.com/")
+  # driver = webdriver.Chrome('/Users/patrickkshih/Desktop/projects/automateproj/chromedriver')
+  # driver.get("https://www.southwest.com/")
 
-	check_in_button = driver.find_element(By.ID, 'booking-form--check-in-tab')
-	check_in_button.click()
+  # check_in_button = driver.find_element(By.ID, 'booking-form--check-in-tab')
+  # check_in_button.click()
 
-	conf_num = driver.find_element(By.ID, 'confirmationNumber')
-	conf_num.send_keys(number)
+  # conf_num = driver.find_element(By.ID, 'confirmationNumber')
+  # conf_num.send_keys(number)
 
-	first = driver.find_element(By.ID, 'firstName')
-	first.send_keys(first_name)
+  # first = driver.find_element(By.ID, 'firstName')
+  # first.send_keys(first_name)
 
-	last = driver.find_element(By.ID, 'lastName')
-	last.send_keys(last_name)
-
-  	driver.find_element(By.ID, 'jb-button-check-in').click()
+  # last = driver.find_element(By.ID, 'lastName')
+  # last.send_keys(last_name)
+  # driver.find_element(By.ID, 'jb-button-check-in').click()
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+  app.run(debug=True)
+  main(sys.argv[1:])
